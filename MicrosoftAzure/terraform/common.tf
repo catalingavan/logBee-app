917b45c98a582b86e31483b97ea55296e899fa3b
Catalin Gavan <catalingavan@gmail.com>
2024-10-16 16:35:54 +0200

    Azure terraform

--- MicrosoftAzure/terraform/common.tf
+++ MicrosoftAzure/terraform/common.tf
@@ -7,7 +7,39 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+  subscription_id = "sdfsdf"
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                = "example-cosmosdb"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.example.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "example" {
+  name                = "example-cosmosdb-sql-db"
+  resource_group_name = azurerm_resource_group.example.name
+  account_name        = azurerm_cosmosdb_account.example.name
+
+  autoscale_settings {
+    max_throughput = 1000
+  }
 }
